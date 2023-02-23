350e02d4f2eee9b7e9c714b423de40c2673cf6c9
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-02-23 18:49:52 -0300

    [FIX]

--- tripe/src/context/context.jsx
+++ tripe/src/context/context.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useContext } from "react";
-import sublinks from './data';
+import sublinks from '../data';
 
 const AppContext = React.createContext();
 
