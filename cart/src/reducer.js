680768a330284c3828184bb6ac06f22ff0363b45
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-02-27 21:01:04 -0300

    [FEAT] fetch data with reducer

--- cart/src/reducer.js
+++ cart/src/reducer.js
@@ -3,34 +3,36 @@ const reducer = (state, action) => {
         return { ...state, cart: [] }
     }
 
-    if(action.type === 'REMOVE') {
-        return {...state, cart:state.cart.filter((carItem) => 
-            carItem.id !== action.payload )} // payload is id
+    if (action.type === 'REMOVE') {
+        return {
+            ...state, cart: state.cart.filter((carItem) =>
+                carItem.id !== action.payload)
+        } // payload is id
     }
 
-    if(action.type === 'INCREASE') {
+    if (action.type === 'INCREASE') {
         let tempCart = state.cart.map((cartItem) => {
-            if(cartItem.id === action.payload) {
-                return { ...cartItem, amount: cartItem.amount + 1}
+            if (cartItem.id === action.payload) {
+                return { ...cartItem, amount: cartItem.amount + 1 }
             }
             return cartItem;
         });
-        return {...state, cart:tempCart}
+        return { ...state, cart: tempCart }
     }
 
-    if(action.type === 'DECREASE') {
+    if (action.type === 'DECREASE') {
         let tempCart = state.cart.map((cartItem) => {
-            if(cartItem.id === action.payload) {
-                return { ...cartItem, amount: cartItem.amount - 1}
+            if (cartItem.id === action.payload) {
+                return { ...cartItem, amount: cartItem.amount - 1 }
             }
             return cartItem;
         }).filter((cartItem) => cartItem.amount !== 0)
-        return {...state, cart:tempCart}
+        return { ...state, cart: tempCart }
     }
 
-    if(action.type === 'GET_TOTALS') {
-        let {total, amount} = state.cart.reduce((cartTotal, cartItem) => {
-            const {price, amount} = cartItem;
+    if (action.type === 'GET_TOTALS') {
+        let { total, amount } = state.cart.reduce((cartTotal, cartItem) => {
+            const { price, amount } = cartItem;
             const itemTotal = price * amount;
 
             cartTotal.total += itemTotal;
@@ -41,8 +43,16 @@ const reducer = (state, action) => {
             amount: 0
         })
         total = parseFloat(total.toFixed(2))
-        
-        return {...state, total, amount}
+
+        return { ...state, total, amount }
+    }
+
+    if (action.type === 'LOADING') {
+        return { ...state, loading: true };
+    }
+
+    if (action.type === 'DISPLAY_ITEMS') {
+        return { ...state, cart: action.payload, loading: false }
     }
 
     return state
